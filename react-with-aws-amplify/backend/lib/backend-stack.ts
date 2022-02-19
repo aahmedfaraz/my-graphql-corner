f5f738757ece47be089840561dfa6788ed602bd6
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-02-19 23:02:35 +0500

    Minors

--- react-with-aws-amplify/backend/lib/backend-stack.ts
+++ react-with-aws-amplify/backend/lib/backend-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
